784d7473b1492b6197deb4c65ae86742ec3e4fd1
ndean73 <Dean.Nalumpa@mlnr.gov.zm>
2020-01-16 09:39:15 -0800

    Task: Added Write Method and Static class

---
+++ csv/writecsv.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using CsvHelper;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+using System.Diagnostics;
+
+
+namespace Csv
+{
+    public class writecsv
+    {
+        
+        public static IList<T> WritecsvFile<T>(IEnumerable<T> countryCapitalQuery,string file_name)
+        {
+            
+            var writePath = "c://csvfiles//" + file_name + ".csv";
+            using (var writer = new StreamWriter(writePath))
+            using (var csv = new CsvWriter(writer))
+            {
+                csv.WriteRecords(countryCapitalQuery);
+            }
+            
+            return null;
+           
+        }
+
+       
+    }
+}
--- csvTests/readcsvTests.cs
+++ csvTests/readcsvTests.cs
@@ -3,6 +3,7 @@ using Csv;
 using CsvHelper;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -16,41 +17,31 @@ namespace csv.Tests
         [TestMethod()]
         public void ReadInCSVTest()
         {
+            string file_name = "capital";
             var path = "c://csvfiles//worldcities.csv";
             var doubleTypeConversion = new DoubleConversion();
             IList<CityModel> myList = ReadCsv.ReadCsvFile<CityModel, CityMap>(path, doubleTypeConversion);
-            var countryCapitalQuery = from s in myList
-                           where s.Capital.Equals("primary")
-                           orderby s.Country ascending
-                           select s;
+            
+            IEnumerable<CityModel> countryCapitalQuery = from s in myList
+                                      where s.Capital.Equals("primary")
+                                      orderby s.Country ascending
+                                      select s;
+           
             // SOme Update
             foreach (CityModel city in countryCapitalQuery)
             {
                 Debug.Write(city.Country + ": " + city.City_name + Environment.NewLine);
             }
-            var queryName = nameof(countryCapitalQuery);
-            var writePath = "c://csvfiles//" +  queryName  + ".csv";
-            using (var writer = new StreamWriter(writePath))
-            using (var csv = new CsvWriter(writer))
-            {
-                csv.WriteRecords(countryCapitalQuery);
-            }
-            Assert.IsTrue(File.Exists(writePath));
+           
+            writecsv.WritecsvFile<CityModel>(countryCapitalQuery, file_name);
 
             var QSCount = (from city in countryCapitalQuery
                            select city).Count();
-
             Debug.Write(QSCount);
 
             Assert.AreEqual(15493, myList.Count());
 
-            // countryQuery = records.Where(city => city.Country.Equals("United States"));
-            /*
-            foreach (CityModel city in countryQuery)
-            {
-                var name = city.City_name.ToString();
-            }
-            */
+            
         }
     }
 }
